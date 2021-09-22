03efb424db9b8c7905e0b148a52edf144f5c66d3
Alexander Winkowski <dereference23@outlook.com>
2021-09-22 17:31:50 +0200

    surya: Update sensors service to 2.1
    Change-Id: Ie417407c36929cf34ecbdd28feb1cab996fe1f30

--- device.mk
+++ device.mk
@@ -74,7 +74,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal
+    android.hardware.sensors@2.1-service.multihal
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
