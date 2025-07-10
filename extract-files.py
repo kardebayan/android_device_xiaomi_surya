2f121a8370593a93e111d7c74274e201e7c9c09c
mikoker <mikolaj.kolata@gmail.com>
2025-07-10 14:47:36 +0200

    surya: Patch `com.vidhance.node.eis.so` with `libui-v34`
    Change-Id: If8651ef34c3212d5fa6f1314e46346406b6761a9
    Signed-off-by: Ritesh <SahanyRitesh@proton.me>
    Signed-off-by: Debayan Kar <kardebayan3@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -61,6 +61,8 @@ blob_fixups: blob_fixups_user_type = {
         .binary_regex_replace(b'megviifacepp_0_5_2_model', b'facedetectpp_0_5_2_model'),
     'vendor/lib64/camera/components/com.qti.node.watermark.so': blob_fixup()
         .add_needed('libpiex_shim.so'),
+    'vendor/lib64/camera/components/com.vidhance.node.eis.so': blob_fixup()
+        .replace_needed('libui.so', 'libui-v34.so'),
     ('vendor/lib64/libalAILDC.so', 'vendor/lib64/libalLDC.so', 'vendor/lib64/libalhLDC.so'): blob_fixup()
         .clear_symbol_version('AHardwareBuffer_allocate')
         .clear_symbol_version('AHardwareBuffer_describe')
